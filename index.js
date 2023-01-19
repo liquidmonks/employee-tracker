0637f47713e05efc8c98d66fdca2033f278ae511
Liquid Monks <info@liquidmonks.com>
2023-01-19 12:02:22 -0600

    index.js includes switch case scenarios for each user choice selection.

--- index.js
+++ index.js
@@ -23,40 +23,97 @@ db.connect(function (err) {
 });
 
 function start() {
-  inquirer.prompt({
-    name: "action",
-    type: "list",
-    message: "What would you like to do?",
-    choices: [
-      new inquirer.Separator("********Department List********"),
-      "View all departments",
-      "Add a department",
-      "Delete a department",
-      "View utilized budget of a department",
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        new inquirer.Separator("********Department List********"),
+        "View all departments",
+        "Add a department",
+        "Delete a department",
+        "View utilized budget of a department",
 
-      new inquirer.Separator("********Role List********"),
-      "View all roles",
-      "Add a role",
-      "Delete a role",
+        new inquirer.Separator("********Role List********"),
+        "View all roles",
+        "Add a role",
+        "Delete a role",
 
-      new inquirer.Separator("********Employee List********"),
-      "View all employees",
-      "View all employees by department",
-      "View all employees by manager",
-      "Add an employee",
-      "Add a manager",
-      "Update an employee role",
-      "Update an employee manager",
-      "Delete an employee",
+        new inquirer.Separator("********Employee List********"),
+        "View all employees",
+        "View all employees by department",
+        "View all employees by manager",
+        "Add an employee",
+        "Add a manager",
+        "Update an employee role",
+        "Update an employee manager",
+        "Delete an employee",
 
-      new inquirer.Separator("********Exit********"),
-      "Exit",
-    ],
-  });
+        new inquirer.Separator("********Exit********"),
+        "Exit",
+      ],
+    })
 
-  // .then(function (answer) {
-  //   switch (answer.action) {
-} // end of start function
+    .then(function (answer) {
+      switch (answer.action) {
+        case "View all departments":
+          viewDepartments();
+          break;
+        case "Add a department":
+          addDepartment();
+          break;
+        case "Delete a department":
+          deleteDepartment();
+          break;
+        case "View utilized budget of a department":
+          viewBudget();
+          break;
+        case "View all roles":
+          viewRoles();
+          break;
+        case "Add a role":
+          addRole();
+          break;
+        case "Delete a role":
+          deleteRole();
+          break;
+        case "View all employees":
+          viewEmployees();
+          break;
+        case "View all employees by department":
+          viewEmployeesByDepartment();
+          break;
+        case "View all employees by manager":
+          viewEmployeesByManager();
+          break;
+        case "Add an employee":
+          addEmployee();
+          break;
+        case "Add a manager":
+          addManager();
+          break;
+        case "Update an employee role":
+          updateEmployeeRole();
+          break;
+        case "Update an employee manager":
+          updateEmployeeManager();
+          break;
+        case "Delete an employee":
+          deleteEmployee();
+          break;
+        case "Exit":
+          exit();
+          break;
+        default:
+          console.log("Please select a valid option!");
+          start();
+          break;
+      } // end of switch
+    }); // end of then function
+} // end of else function
+
+// end of start function
 
 // // Function to view all departments
 // function viewDepartments() {
